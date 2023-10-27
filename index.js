2cdf671e111d0faba8772593716b7997cd5512e9
Roc <lostlegacymaster@gmail.com>
2023-10-26 19:13:47 -0600

    refactor(login): use logger util to log the login

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once)
 client.once(Events.ClientReady, (c) => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
+    logger(
+        "Bot",
+        "Login",
+        "Info",
+        null,
+        `Logged in as ${c.user.tag.magenta}`
+    )
 });
 
 // Status change function
