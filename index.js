be646c303fbb0cccdd4524d8416b25844f975e38
Roc <lostlegacymaster@gmail.com>
2023-10-26 22:11:23 -0600

    feat(command): add command loader and handler

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Files
+const fs = require('node:fs');
+const path = require('node:path');
 // Config
 const config = require("./utils/config");
 const { token } = require("./token.json");
@@ -5,8 +8,9 @@ const { token } = require("./token.json");
 const {
     Client,
     Events,
-    GatewayIntentBits,
+    Collection,
     ActivityType,
+    GatewayIntentBits,
 } = require("discord.js");
 // Other packages
 const { logger } = require("./utils/roc-logger");
@@ -14,14 +18,54 @@ const { logger } = require("./utils/roc-logger");
 // Create a new client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+// Command Collection
+client.commands = new Collection();
+
+// Load Commands
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// Handle interactions
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
 // When the client is ready, run this code (only once)
 client.once(Events.ClientReady, (c) => {
-    logger(
-        "Bot",
-        "Login",
-        "Info",
-        `Logged in as ${c.user.tag.magenta}`
-    )
+    logger("Bot", "Login", "Info", `Logged in as ${c.user.tag.magenta}`);
 });
 
 // Status change function
