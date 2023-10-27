7eec0957ffb4c540e5015012ef9f902daa0292d9
Roc <lostlegacymaster@gmail.com>
2023-10-27 16:18:34 -0600

    feat(command): add set-welcome-role command

--- commands/channels/set-welcome-role.js
+++ commands/channels/set-welcome-role.js
@@ -1,12 +1,47 @@
 const config = require("../../utils/config");
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const {
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+    EmbedBuilder,
+} = require("discord.js");
+const WelcomeSettings = require("../../models/welcomeSettings");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("set-welcome-role")
         .setDescription("Sets the server's welcome role")
+        .addRoleOption((option) =>
+            option
+                .setName("role")
+                .setDescription("The role to set as the welcome role")
+                .setRequired(true)
+        )
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
     async execute(interaction) {
-        await interaction.reply("Test");
+        const role = await interaction.options.getRole("role");
+
+        const [settings, created] = await WelcomeSettings.findOrCreate({
+            where: { guildId: interaction.guild.id },
+        });
+
+        await settings.update({ welcomeRole: role.id, enabled: true });
+
+        const embed = new EmbedBuilder()
+            .setColor(config.colors.primary)
+            .setTitle("Role Set!")
+            .setDescription(
+                `Your welcome role has been set!\n\
+            > ${role}`
+            )
+            .setFooter({
+                text: `Requested by: ${interaction.user.tag}`,
+                iconURL: `${interaction.user.displayAvatarURL({
+                    format: "png",
+                    dynamic: true,
+                    size: 1024,
+                })}`,
+            });
+
+        await interaction.reply({ embeds: [embed], ephemeral: true });
     },
 };
