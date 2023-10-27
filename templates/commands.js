2a359e05f84b3feb245f6b0c29b58fc9cc89e8f1
Roc <lostlegacymaster@gmail.com>
2023-10-26 18:55:57 -0600

    feat(commands): add template folder

---
+++ templates/commands.js
@@ -0,0 +1,11 @@
+const config = require("../utils/config");
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("")
+        .setDescription(""),
+    async execute(interaction) {
+        await interaction.reply("Pong!");
+    },
+};
