17703b2b1180071f36aee3bcc4d84caa15369775
Roc <lostlegacymaster@gmail.com>
2023-10-28 15:06:01 -0600

    fix(template): fix template

--- templates/event.js
+++ templates/event.js
@@ -1,11 +1,12 @@
 const { logger } = require("../utils/roc-logger");
 const config = require("../utils/config");
+const lang = require("../utils/lang");
 const { Events } = require('discord.js');
 
 module.exports = {
 	name: Events.<eventType>,
 	// once: true,
-	execute(client) {
+	async execute(client) {
 		// Code
 	},
 };
