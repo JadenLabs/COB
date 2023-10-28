05553f065da2784df135d5ea5c9ab18b93631946
Roc <lostlegacymaster@gmail.com>
2023-10-28 16:44:28 -0600

    refactor(template): add packages & format

--- templates/command.js
+++ templates/command.js
@@ -1,10 +1,10 @@
+const { logger } = require("../utils/roc-logger");
 const config = require("../../utils/config");
+const lang = require("../utils/lang");
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("")
-        .setDescription(""),
+    data: new SlashCommandBuilder().setName("").setDescription(""),
     async execute(interaction) {
         await interaction.reply("Pong!");
     },
