9144ac0ad9723c705c8389bbf21b60b7e95cac9f
Roc <lostlegacymaster@gmail.com>
2023-10-29 16:46:26 -0600

    feat(command): add partnership ad command

---
+++ commands/network/partnership-ad.js
@@ -0,0 +1,51 @@
+const { logger } = require("../../utils/roc-logger");
+const config = require("../../utils/config");
+const lang = require("../../utils/lang");
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const NetworkSettings = require("../../models/networkSettings");
+const ReplyListener = require("../../models/replyListener");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("partnership-ad")
+        .setDescription("Set or view this server's ad"),
+    async execute(interaction) {
+        // Get settings
+        const [networkSettings, networkCreated] =
+            await NetworkSettings.findOrCreate({
+                where: { guildId: interaction.guild.id },
+            });
+
+        // Strings
+        const adSet = networkSettings.serverAd ? "Set" : "None";
+        const viewLine = networkSettings.serverAd
+            ? "Use the button below to view the server's ad\n"
+            : "";
+
+        // Embed
+        const embed = new EmbedBuilder()
+            .setColor(config.colors.secondary)
+            .setTitle("Server Ad")
+            .setDescription(
+                `${viewLine}**Reply** to this message with your server's ad to set it.`
+            )
+            .setFooter({
+                text: `Requested by: ${interaction.user.tag}`,
+                iconURL: `${interaction.user.displayAvatarURL({
+                    format: "png",
+                    dynamic: true,
+                    size: 1024,
+                })}`,
+            });
+
+        const response = await interaction.reply({ embeds: [embed] });
+        const responseMessage = await response.fetch();
+
+        // Create listener
+        const listener = await ReplyListener.create({
+            guildId: interaction.guild.id,
+            userId: interaction.user.id,
+            messageId: responseMessage.id,
+        });
+    },
+};
