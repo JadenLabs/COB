059ce0d99123a53ddebbd3f6b59766bcf5313d89
Roc <lostlegacymaster@gmail.com>
2023-10-29 01:39:56 -0600

    feat(script): update sync-db

--- scripts/sync-db.js
+++ scripts/sync-db.js
@@ -1,6 +1,7 @@
 /*
 const Model = require("./models/model"); 
 */
+const OutgoingRequests = require("../models/outgoingRequests");
 const WelcomeSettings = require("../models/welcomeSettings");
 const NetworkSettings = require("../models/networkSettings");
 const Partnerships = require("../models/partnerships");
@@ -10,6 +11,7 @@ const Partnerships = require("../models/partnerships");
 /*
 Model.sync({ alter: true });
 */
+OutgoingRequests.sync({ alter: true });
 WelcomeSettings.sync({ alter: true });
 NetworkSettings.sync({ alter: true });
 Partnerships.sync({ alter: true });
