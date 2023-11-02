14c6459110274ef2bf9928267eef09e8d31bd6c9
Roc <lostlegacymaster@gmail.com>
2023-11-01 19:57:09 -0600

    feat(script): update sync-db

--- scripts/sync-db.js
+++ scripts/sync-db.js
@@ -1,11 +1,13 @@
 /*
 const Model = require("./models/model"); 
 */
+const InterviewSettings = require("../models/interviewSettings");
 const OutgoingRequests = require("../models/outgoingRequests");
 const WelcomeSettings = require("../models/welcomeSettings");
 const NetworkSettings = require("../models/networkSettings");
 const ReplyListener = require("../models/replyListener");
 const Partnerships = require("../models/partnerships");
+const Interviews = require("../models/interviews");
 const Admins = require("../models/admins");
 const Bans = require("../models/bans");
 
@@ -14,10 +16,12 @@ const Bans = require("../models/bans");
 /*
 Model.sync({ alter: true });
 */
+InterviewSettings.sync({ alter: true });
 OutgoingRequests.sync({ alter: true });
 WelcomeSettings.sync({ alter: true });
 NetworkSettings.sync({ alter: true });
 ReplyListener.sync({ alter: true });
 Partnerships.sync({ alter: true });
+Interviews.sync({ alter: true });
 Admins.sync({ alter: true });
 Bans.sync({ alter: true });
