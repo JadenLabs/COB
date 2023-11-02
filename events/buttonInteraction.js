7f6c918ee42d3afdad976c5dca81cc23ea995583
Roc <lostlegacymaster@gmail.com>
2023-11-01 20:19:15 -0600

    feat(event): add network settings panel

--- events/buttonInteraction.js
+++ events/buttonInteraction.js
@@ -10,6 +10,7 @@ const {
 } = require("discord.js");
 const WelcomeSettings = require("../models/welcomeSettings");
 const NetworkSettings = require("../models/networkSettings");
+const InterviewSettings = require("../models/interviewSettings");
 
 module.exports = {
     name: Events.InteractionCreate,
@@ -40,7 +41,6 @@ module.exports = {
             const joinRole = welcomeSettings.joinRole
                 ? `<@&${welcomeSettings.joinRole}>`
                 : "None";
-                
 
             // Embed
             const welcomePage = new EmbedBuilder()
@@ -303,6 +303,112 @@ module.exports = {
                 embeds: [embed],
                 ephemeral: true,
             });
+        } else if (interaction.customId === "interview_settings_button") {
+            // Databases
+            const [interviewSettings, created] =
+                await InterviewSettings.findOrCreate({
+                    where: { guildId: interaction.guild.id },
+                });
+
+            // Strings
+            const interviewEnabled =
+                interviewSettings.enabled === true ? "Enabled" : "Disabled";
+            const interviewEnabledEmoji =
+                interviewSettings.enabled === true
+                    ? lang.E.greenTick
+                    : lang.E.redCross;
+
+            // Embed
+            const interviewPage = new EmbedBuilder()
+                .setColor(config.colors.secondary)
+                .setTitle(`💬 | Interview Settings`)
+                .setDescription(
+                    `Use the buttons below to enable or disable this module.\n\n\
+            **Commands**\n\
+            > Welcome Channel:\n\
+            > ${lang.E.reply} </set-welcome-channel:1167548439324803286>\n\
+
+            **Status**\n\
+            > Enabled:\n\
+            > ${lang.E.reply} ${interviewEnabledEmoji} \`${interviewEnabled}\``
+                )
+                .setFooter({
+                    text: `Requested by: ${interaction.user.tag}`,
+                    iconURL: `${interaction.user.displayAvatarURL({
+                        format: "png",
+                        dynamic: true,
+                        size: 1024,
+                    })}`,
+                });
+
+            // Buttons
+            let enable = new ButtonBuilder()
+                .setCustomId("enable_interview_button")
+                .setLabel("Enable")
+                .setStyle(ButtonStyle.Success);
+
+            let disable = new ButtonBuilder()
+                .setCustomId("disable_interview_button")
+                .setLabel("Disable")
+                .setStyle(ButtonStyle.Danger);
+
+            if (interviewSettings.enabled === true) {
+                enable.setDisabled(true);
+                disable.setDisabled(false);
+            } else {
+                enable.setDisabled(false);
+                disable.setDisabled(true);
+            }
+
+            // Action row
+            const row = new ActionRowBuilder().addComponents(enable, disable);
+
+            // Response
+            return interaction.reply({
+                embeds: [interviewPage],
+                components: [row],
+                ephemeral: true,
+            });
+        } else if (interaction.customId === "enable_interview_button") {
+            // Get DB
+            const [interviewSettings, created] =
+                await InterviewSettings.findOrCreate({
+                    where: { guildId: interaction.guild.id },
+                });
+
+            if (interviewSettings.enabled === true) {
+                return interaction.reply({
+                    content: `This module has already been enabled!`,
+                    ephemeral: true,
+                });
+            } else {
+                await interviewSettings.update({ enabled: true });
+
+                return interaction.reply({
+                    content: `${lang.E.greenTick} The interview module has been enabled!`,
+                    ephemeral: true,
+                });
+            }
+        } else if (interaction.customId === "disable_interview_button") {
+            // Get DB
+            const [interviewSettings, created] =
+                await InterviewSettings.findOrCreate({
+                    where: { guildId: interaction.guild.id },
+                });
+
+            if (networkSettings.enabled === true) {
+                await networkSettings.update({ enabled: false });
+
+                return interaction.reply({
+                    content: `${lang.E.redCross} The interview module has been disabled!`,
+                    ephemeral: true,
+                });
+            } else {
+                return interaction.reply({
+                    content: `This module has already been disabled!`,
+                    ephemeral: true,
+                });
+            }
         }
     },
 };
